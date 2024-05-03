88502f536745230e2bb5c53afed1f68d2259a906
farid_sadreddinov <faridsadreddinov@yandex.ru>
2024-05-03 14:27:38 +0300

    Finished project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sadreddinov/go-rabbitmq-learning
+
+go 1.21.0
+
+require github.com/rabbitmq/amqp091-go v1.9.0
---
+++ recieve/recieve.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s\n", d.Body)
+		}
+	}()
+
+	log.Printf("[*] Waiting for messages. To exit press CTRL + C")
+	<-forever
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
